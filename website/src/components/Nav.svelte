6e492186c4961201ce3aa33aa9043de461578710
Tal Shiri <eiopaa@gmail.com>
2020-10-06 22:02:13 -0700

    [website] navbar takes you to exhibits

--- website/src/components/Nav.svelte
+++ website/src/components/Nav.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	export let segment: string;
 </script>
 
 <style>
@@ -8,54 +7,10 @@
 		font-weight: 300;
 		padding: 0 1em;
 	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
 </style>
 
 <nav class="bg-white text-black fixed w-full">
-	<!-- <ul>
-		<li><a aria-current="{segment === undefined ? 'exhibits' : undefined}" href=".">exhibits</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
- -->
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<!-- <li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul> -->
-	<div class="text-4xl p-2 font-normal select-none">Derivative Work</div>
+	<a href="/exhibits">
+		<div class="text-4xl p-2 font-normal select-none">Derivative Work</div>
+	</a>
 </nav>
