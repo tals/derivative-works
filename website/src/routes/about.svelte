2a515db35418d91f58deed44ed0b705ab5885db2
joel-simon <joelsimon6@gmail.com>
2020-10-09 12:01:53 -0700

    added more text to the about page

--- website/src/routes/about.svelte
+++ website/src/routes/about.svelte
@@ -5,25 +5,31 @@
   a {
     text-decoration: underline;
   }
+  p {
+    margin-bottom: 1em;
+  }
 </style>
 <div class="p-4 text-black flex justify-center">
-	<div class="max-w-3xl">
-    <p>
+	<div class="max-w-3xl ">
+    <p class="text-right">
       Derivative-Works is an experiment in using machine learning to create image collages.
-      The algorithm cuts patches out from reference images and rearranges them to optimize an objective. Here, the reference images are created in Artbreeder using BigGan and optimized to satisfy a neural network that detects faces. Each run produces a new and distinct face. 
+      Reference images are given to the algorithm which cuts out and rearranges patches to optimize an objective.  Here, the references are public domain images created in 
+      <a href='https://www.Artbreeder.com/' target='_blank'>Artbreeder </a>
+      using BigGan and optimized to satisfy a neural network that detects faces. Each run produces a new and distinct face. 
     </p>
-    <!-- TODO: say a little more about future work and videos. -->
-    <br>
+    <p>
+      The popularity of generative ML and GAN’s have created an infinite abundance of textures. Derivative-Works thinks of these textures as ingredients and raw materials to reinvent, similar to the prior Dada use of magazines and print media. The arrangement of dozens of textures can become an approachable creative medium driven by the selection of images and objectives.
+    </p>
+    <p>
+      A patch generator GAN was first trained on layered Perlin noise to be able to create arbitrary shapes. Each collage is represented by a series of patch latents and transformation matrices for those patches. These variables are then optimized to do feature inversion over a face classifier (DLIB’s CNN model) by doing gradient descent on the classifier features.
+    The primary difference between this method and vanilla inversion is the input medium: instead of optimizing pixels directly, we optimize collage collage parameters, leading to a variety of textures and compositions. The videos on the website show the actual optimization.
+    </p>
     <p>
       Created by
       <a href='https://www.joelsimon.net/' target='_blank'>Joel</a>
       &
       <a href='https://github.com/tals' target='_blank'>Tal</a>
     </p>
-    <br>
     <p>Code is <a href='https://github.com/tals/derivative-work' target='_blank'>available here</a> </p>
-    
     </div>
-
 </div>
-
