7b8f469e13b5a410a517cf36bf22960fd7b9c4e3
Tal Shiri <eiopaa@gmail.com>
2020-10-10 00:51:29 -0700

    [website] refactor + fix mobile

---
+++ website/src/routes/exhibits/_exhibitMini.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+  import { getPreviewImage, getVideoUrl } from "../../url_utils";
+  import * as dt from "../../dataTypes";
+  export let exhibit: dt.Collage;
+  import IntersectionObserver from "./_intersectionObserver.svelte";
+
+
+  let videoElement: HTMLVideoElement;
+
+  let scrollX: number;
+  let scrollY: number;
+  let innerWidth: number;
+  let innerHeight: number;
+  let hasPlayed = false;
+  $: autoplay = innerWidth < 768;
+
+  function playVideo() {
+		if (!hasPlayed) {
+      hasPlayed = true
+			videoElement.src = getVideoUrl(exhibit);
+			videoElement.oncanplay = () => videoElement.play()
+		}
+  }
+
+  function updateVisibility(innerHeight) {
+    const rect = videoElement.getBoundingClientRect()
+    const fullyVisible =
+      rect.top >= 0 &&
+      rect.bottom <= innerHeight;
+
+    const fullyHidden =
+      rect.bottom <= 0 ||
+      rect.top >= innerHeight ;
+
+    if (fullyVisible) {
+      playVideo()
+    }
+    if  (fullyHidden) {
+      if (exhibit.key == "dzdsdu") {
+      }
+      videoElement.src = null;
+      hasPlayed = false;
+    }
+  }
+
+  function onScroll() {
+    updateVisibility(innerHeight);
+  }
+
+  $: if (autoplay) {
+    updateVisibility(innerHeight);
+  }
+</script>
+
+<style>
+  .exhibit {
+    /* width: 256px; */
+    max-width: calc(50% - 4px);
+    /* width: 100%; */
+    margin: 1px;
+  }
+
+  /* Medium (md) */
+  @media (min-width: 768px) {
+    .exhibit {
+      width: 256px;
+    }
+  }
+
+  .exhibit img,
+  .exhibit video {
+    background: white;
+    width: 100%;
+  }
+</style>
+
+<svelte:window
+  on:scroll={onScroll}
+  bind:scrollX
+  bind:scrollY
+  bind:innerWidth
+  bind:innerHeight />
+
+<a rel="prefetch" href="/exhibits/{exhibit.key}" class="m-0 exhibit relative">
+  <figure>
+    <video
+      muted
+      autoplay
+      playsinline
+      webkit-playsinline
+      poster={getPreviewImage(exhibit)}
+      bind:this={videoElement}
+      on:mouseover={playVideo} />
+  </figure>
+</a>
--- website/src/routes/exhibits/index.svelte
+++ website/src/routes/exhibits/index.svelte
@@ -1,76 +1,14 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { EXHIBITS } from "../../data";
-	import { getPreviewImage, getVideoUrl } from "../../url_utils";
-	
-	function onmouseover() {
-		if (!this.hasplayed) {
-			this.src = this.dataset.src
-			this.hasplayed = true
-			this.oncanplay = () => this.play()
-		}
-	}
-	function inViewport (el: HTMLElement) {
-		var rect = el.getBoundingClientRect()
-		return (
-			rect.top >= 0 &&
-			rect.left >= 0 &&
-			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-			rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
-		);
-	}
-	onMount(() => {
-		const videos:HTMLVideoElement[] = [...document.querySelectorAll('video')]
-		window.onscroll = () => {			
-			if (window.innerWidth < 768) {
-				for (const vid of videos) {
-					if (inViewport(vid)) { onmouseover.call(vid) }
-				}
-			}
-		}
-		window.onscroll()
-	})
+	import ExhibitMini from "./_exhibitMini.svelte";
+
+
 
 </script>
 
-<style>
-	.exhibit {
-		/* width: 256px; */
-		max-width: calc(50% - 4px);
-		/* width: 100%; */
-		margin: 1px;
-	}
-
-	/* Medium (md) */
-	@media (min-width: 768px) { 
-		.exhibit {
-			width: 256px;
-		}
-	 }
-
-
-	.exhibit img, .exhibit video {
-		background: white;
-		width: 100%;
-	}
-</style>
-  
 <div class="flex text-white justify-center flex-wrap">
-  {#each EXHIBITS as exhibit}
-   		 <!-- we're using the non-standard `rel=prefetch` attribute to
-		tell Sapper to load the data for the page as soon as
-		the user hovers over the link or taps it, instead of
-		waiting for the 'click' event -->
-		<a rel=prefetch href="/exhibits/{exhibit.key}" class="m-0 exhibit relative">
-			<figure>
-			 <video 
-				muted autoplay
-				playsinline webkit-playsinline
-				poster={getPreviewImage(exhibit)}
-				on:mouseover={onmouseover}
-				data-src={getVideoUrl(exhibit)}>
-			 </video>
-		</figure>
-	</a>
+	{#each EXHIBITS as exhibit}
+		<ExhibitMini {exhibit}/>
   {/each}
 </div>
